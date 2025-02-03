1ac049fa915fa2cfc5b2b2b674b10c84be45c48c
June Zyrus <98371541+ItsMeYobs@users.noreply.github.com>
2025-02-03 09:26:19 +0800

    Update lexicalAnalyzer.c

--- lexicalAnalyzer.c
+++ lexicalAnalyzer.c
@@ -1,4 +1,4 @@
-#include <ctype.h>
+#include <ctype.h> 
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -6,6 +6,7 @@
 
 #define MAX_LENGTH 100
 
+// Function to check if a character is a delimiter
 bool isDelimiter(char chr) {
     return (chr == ' ' || chr == '+' || chr == '-' ||
             chr == '*' || chr == '/' || chr == ',' ||
@@ -15,17 +16,20 @@ bool isDelimiter(char chr) {
             chr == '{' || chr == '}');
 }
 
+// Function to check if a character is an operator
 bool isOperator(char chr) {
     return (chr == '+' || chr == '-' || chr == '*' ||
             chr == '/' || chr == '>' || chr == '<' ||
             chr == '=');
 }
 
+// Function to check if a character is a parenthesis
 bool isParenthesis(char chr) {
     return (chr == '{' || chr == '}' || chr == '[' || 
             chr == ']' || chr == '(' || chr == ')');
 }
 
+// Function to check if a string is a valid identifier
 bool isValidIdentifier(char* str) {
     return (str[0] != '0' && str[0] != '1' && str[0] != '2' &&
             str[0] != '3' && str[0] != '4' && str[0] != '5' &&
@@ -33,6 +37,7 @@ bool isValidIdentifier(char* str) {
             str[0] != '9' && !isDelimiter(str[0]));
 }
 
+// Function to check if a string is a keyword
 bool isKeyword(char* str) {
     const char* keywords[] = {
         "auto", "break", "case", "char", "const", "continue",
@@ -51,6 +56,7 @@ bool isKeyword(char* str) {
     return false;
 }
 
+// Function to check if a string is a constant
 bool isConstant(char* str) {
     if (str == NULL || *str == '\0') {
         return false;
@@ -62,6 +68,7 @@ bool isConstant(char* str) {
     return str[i] == '\0';
 }
 
+// Function to extract a substring from the given string
 char* getSubstring(char* str, int start, int end) {
     int subLength = end - start + 1;
     char* subStr = (char*)malloc((subLength + 1) * sizeof(char));
@@ -70,49 +77,55 @@ char* getSubstring(char* str, int start, int end) {
     return subStr;
 }
 
+// Function to analyze the lexical structure of the input
 void lexicalAnalyzer(char* input, FILE* file) {
-    int left = 0, right = 0;
     int len = strlen(input);
+    int left = 0;
 
     fprintf(file, "\n%-15s %-15s\n", "Lexeme", "Token");
     fprintf(file, "--------------------------------\n");
-    
-    while (right <= len && left <= right) {
-        if (!isDelimiter(input[right]))
-            right++;
 
-        if (isDelimiter(input[right]) && left == right) {
-            if (isOperator(input[right])) {
-                fprintf(file, "%-15c %-15s\n", input[right], "Operator");
-            } else if (isParenthesis(input[right])) {
-                fprintf(file, "%-15c %-15s\n", input[right], "Parenthesis");
+    // Traverse input character by character
+    for (int right = 0; right <= len; right++) {
+        // If a delimiter (whitespace or end of string) is encountered, process the lexeme
+        if (input[right] == ' ' || input[right] == '\0') {
+            if (right > left) { // Skip if there's no lexeme (empty space)
+                char* subStr = getSubstring(input, left, right - 1);
+
+                // Check the type of lexeme
+                if (isKeyword(subStr)) {
+                    fprintf(file, "%-15s %-15s\n", subStr, "Keyword");
+                } else if (isConstant(subStr)) {
+                    fprintf(file, "%-15s %-15s\n", subStr, "Constant");
+                } else if (isValidIdentifier(subStr)) {
+                    fprintf(file, "%-15s %-15s\n", subStr, "Identifier");
+                } else {
+                    fprintf(file, "%-15s %-15s\n", subStr, "Unidentified");
+                }
+
+                free(subStr);
             }
-            right++;
-            left = right;
-        } else if (isDelimiter(input[right]) && left != right ||
-                   (right == len && left != right)) {
-            char* subStr = getSubstring(input, left, right - 1);
 
-            if (isKeyword(subStr)) {
-                fprintf(file, "%-15s %-15s\n", subStr, "Keyword");
-            } else if (isConstant(subStr)) {
-                fprintf(file, "%-15s %-15s\n", subStr, "Constant");
-            } else if (isValidIdentifier(subStr) &&
-                       !isDelimiter(input[right - 1])) {
-                fprintf(file, "%-15s %-15s\n", subStr, "Identifier");
-            } else if (!isValidIdentifier(subStr) &&
-                       !isDelimiter(input[right - 1])) {
-                fprintf(file, "%-15s %-15s\n", subStr, "Unidentified"); 
+            // Move left to the next character after the delimiter
+            left = right + 1;
+        } else if (isDelimiter(input[right])) {
+            // Handle single-character tokens (operators, parentheses)
+            char* subStr = getSubstring(input, right, right);
+            if (isOperator(input[right])) {
+                fprintf(file, "%-15s %-15s\n", subStr, "Operator");
+            } else if (isParenthesis(input[right])) {
+                fprintf(file, "%-15s %-15s\n", subStr, "Parenthesis");
             }
             free(subStr);
-            left = right;
         }
     }
 }
 
+// Main function to read input and run the lexical analyzer
 int main() {
     char code_input[MAX_LENGTH];
     FILE* file = fopen("output.txt", "w");
+
     if (!file) {
         printf("Error opening file!\n");
         return 1;
@@ -121,10 +134,11 @@ int main() {
     printf("Enter a line of code: ");
     fgets(code_input, MAX_LENGTH, stdin);
     code_input[strcspn(code_input, "\n")] = '\0'; // Remove newline character
-    lexicalAnalyzer(code_input, file);
+
+    lexicalAnalyzer(code_input, file); 
 
     fclose(file);
     system("notepad output.txt");
+
     return 0;
 }
-
